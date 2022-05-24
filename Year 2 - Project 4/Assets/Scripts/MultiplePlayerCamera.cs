ff5132906ee792bb71ace2369dd0a018238aec34
Moks <wierdamax@gmail.com>
2022-05-24 10:47:24 +0200

    School changes
    Laptop changes

--- Year 2 - Project 4/Assets/Scripts/MultiplePlayerCamera.cs
+++ Year 2 - Project 4/Assets/Scripts/MultiplePlayerCamera.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Camera))]
 public class MultiplePlayerCamera : MonoBehaviour
 {
 
     public List<Transform> targets;
+    public float smoothTime = .5f;
+
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
 
     public Vector3 offset;
+
+    private Vector3 velocity;
+    private Camera cam;
+
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
 
 
     void LateUpdate()
@@ -15,11 +32,35 @@ public class MultiplePlayerCamera : MonoBehaviour
         if (targets.Count == 0)
             return;
 
+        Move();
+        Zoom();
+    }
+
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / 50f);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+    void Move()
+    {
         Vector3 centerPoint = GetCenterPoint();
 
         Vector3 newPosition = centerPoint + offset;
 
-        transform.position = newPosition;
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
     }
 
     Vector3 GetCenterPoint()
