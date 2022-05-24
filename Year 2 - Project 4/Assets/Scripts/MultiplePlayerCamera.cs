98208e25cfba457b2cbb3ef96d6d173bac5c4242
edwin <edwinslottje@gmail.com>
2022-05-24 14:41:27 +0200

    Camera Fix, PowerUp Spawning and prefabs

--- Year 2 - Project 4/Assets/Scripts/MultiplePlayerCamera.cs
+++ Year 2 - Project 4/Assets/Scripts/MultiplePlayerCamera.cs
@@ -2,25 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class MultiplePlayerCamera : MonoBehaviour
 {
-
     public List<Transform> targets;
 
     public Vector3 offset;
+    public float smoothTime = .5f;
+
+    public float maxZoom = 40f;
+    public float minZoom = 10f;
+    public float zoomLimiter = 50f;
+
+    private Vector3 velocity;
+    private Camera cam;
     private GameObject[] players;
 
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
 
     void LateUpdate()
     {
         if (targets.Count == 0)
             return;
 
+        Move();
+        Zoom();
+    }
+
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, newZoom, Time.deltaTime);
+    }
+
+    void Move()
+    {
         Vector3 centerPoint = GetCenterPoint();
 
         Vector3 newPosition = centerPoint + offset;
 
-        transform.position = newPosition;
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
     }
 
     Vector3 GetCenterPoint()
@@ -31,26 +66,22 @@ public class MultiplePlayerCamera : MonoBehaviour
         }
 
         var bounds = new Bounds(targets[0].position, Vector3.zero);
-        for(int i =0; i <targets.Count; i++)
+        for (int i = 0; i < targets.Count; i++)
         {
             bounds.Encapsulate(targets[i].position);
         }
 
         return bounds.center;
     }
-
     private void Update()
     {
         players = GameObject.FindGameObjectsWithTag("Player");
         foreach (GameObject player in players)
         {
-            targets.Add(player.transform);
-        }
-        Debug.Log(targets.Count);
-
-        if (targets.Count > 4)
-        {
-            targets.RemoveAt(5);
+            if (targets.Count < players.Length)
+            {
+                targets.Add(player.transform);
+            }
         }
     }
 }
