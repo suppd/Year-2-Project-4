367d89cf96ce3790e56786b331ae7990cbe3f042
MoksHome <wierdamax@gmail.com>
2022-06-07 23:01:08 +0200

    Fixed small on hit bug

--- Year 2 - Project 4/Assets/Scripts/healthBaR.cs
+++ Year 2 - Project 4/Assets/Scripts/healthBaR.cs
@@ -1,25 +1,25 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 
-// public class healthBar : MonoBehaviour
-// {
+public class healthBar : MonoBehaviour
+{
 
 
-//     public Slider slider;
+    public Slider slider;
 
 
-//     public void SetMaxHealth(int health)
-//     {
-//         slider.maxValue = health;
-//         slider.value = health;
-//     }
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
 
 
-//     public void SetHealth(int health)
-//     {
-//         slider.value = health;
-//     }
-// }
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
