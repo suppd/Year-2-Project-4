3ec055bb4bb0c84e6b51f90f8ea4972945464349
edwin <Edwinslottje@gmail.com>
2022-06-08 00:02:57 +0200

    Result screen add

--- Year 2 - Project 4/Assets/ResultsManager.cs
+++ Year 2 - Project 4/Assets/ResultsManager.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class RoundOverManager : MonoBehaviour
+public class ResultsManager : MonoBehaviour
 {
     PlayerConfiguration[] playerConfigs;
     public Canvas canvas;
     public Text text;
     public GameObject prefab;
-    public string Levelname = "LevelDesign2v2";
 
     private int numberOfPlayers = 0;
     private List<GameObject> scores = new List<GameObject>();
     void Awake()
     {
         playerConfigs = PlayerConfigurationManager.Instance.GetPlayerConfigs().ToArray();
-        //Debug.Log(playerConfigs.Length);
 
         for (int i = 0; i < playerConfigs.Length; i++)
         {
@@ -26,11 +24,7 @@ public class RoundOverManager : MonoBehaviour
                 AddScoreBoard();
                 UpdateScoreBoard(playerConfigs[i].playerScore, i, playerConfigs[i].playerIndex, true);
                 Debug.Log("Round Over");
-                text.text = "Player " + playerConfigs[i].playerIndex.ToString() + " Won the Round ! " + "the player's score is now " + playerConfigs[i].playerScore.ToString();
-                if (playerConfigs[i].playerScore >= 5)
-                {
-                    ToResultScreen();
-                }
+                text.text = "Player " + playerConfigs[i].playerIndex.ToString() + " Won Game Round ! " + "the player's score is now " + playerConfigs[i].playerScore.ToString();
             }
             else if (!playerConfigs[i].isAlive)
             {
@@ -42,8 +36,8 @@ public class RoundOverManager : MonoBehaviour
         }
     }
     public void AddScoreBoard()
-    {      
-        var board = Instantiate(prefab,new Vector3(canvas.transform.position.x, canvas.transform.position.y - (75 * numberOfPlayers), canvas.transform.position.z), canvas.transform.rotation, canvas.transform);
+    {
+        var board = Instantiate(prefab, new Vector3(canvas.transform.position.x, canvas.transform.position.y - (75 * numberOfPlayers), canvas.transform.position.z), canvas.transform.rotation, canvas.transform);
         scores.Add(board);
     }
     public void UpdateScoreBoard(int score, int boardInstance, int playerIndex, bool wasAlive)
@@ -51,10 +45,6 @@ public class RoundOverManager : MonoBehaviour
         scores[boardInstance].GetComponent<ScoreBoard>().playerScore = score;
         scores[boardInstance].GetComponent<ScoreBoard>().playerIndex = playerIndex;
         scores[boardInstance].GetComponent<ScoreBoard>().wasAlive = wasAlive;
-    }
-    public void NextLevel()
-    {
-        SceneManager.LoadScene(Levelname);
     }
 
     public void ToResultScreen()
--- Year 2 - Project 4/Assets/Scripts/Player Specific/PlayerStats.cs
+++ Year 2 - Project 4/Assets/Scripts/Player Specific/PlayerStats.cs
@@ -40,7 +40,7 @@ public class PlayerStats : MonoBehaviour
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
         if (HP <= 0)
         {
