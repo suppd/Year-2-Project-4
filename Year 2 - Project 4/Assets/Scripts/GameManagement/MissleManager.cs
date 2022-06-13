db1cbc53c9439bf93d241fd2f1f1f5869aad41e0
edwin <edwinslottje@gmail.com>
2022-06-13 10:18:11 +0200

    Missles working

--- Year 2 - Project 4/Assets/Scripts/GameManagement/MissleManager.cs
+++ Year 2 - Project 4/Assets/Scripts/GameManagement/MissleManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MissleManager : MonoBehaviour
 {
-    public Transform[] spawnPoints;
+    public Transform[] targetPoints;
     public GameObject missle;
     [HideInInspector]
     public int missleNum;
@@ -20,13 +20,10 @@ public class MissleManager : MonoBehaviour
     }
     public void SpawnMissle()
     {
-        //for(int i = 0; i < spawnPoints.Length; i++)
-        foreach(Transform t in spawnPoints) 
-        {
-            
-            Instantiate(missle, this.transform);
-            gameObject.GetComponentInChildren<HomingMissle>().SetTarget(t.gameObject);
-
+        foreach(Transform t in targetPoints) 
+        {         
+            GameObject newMissile = Instantiate(missle, this.transform);
+            newMissile.GetComponent<HomingMissle>().SetTarget(t.gameObject);
         }
         
     }
