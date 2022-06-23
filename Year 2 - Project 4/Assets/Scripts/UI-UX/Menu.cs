55a6be7c581998910ae71b99bb5e634b62ad340c
Oebe Rademaker <499574@student.saxion.nl>
2022-06-23 17:19:05 +0200

    Eggsplanation content added

--- Year 2 - Project 4/Assets/Scripts/UI-UX/Menu.cs
+++ Year 2 - Project 4/Assets/Scripts/UI-UX/Menu.cs
@@ -27,8 +27,6 @@ public class Menu : MonoBehaviour, ISelectHandler, IDeselectHandler, ICancelHand
     public void OnSelect(BaseEventData eventData)
     {
         FindObjectOfType<AudioManager>().Play("MenuHover");
-
-        Debug.Log(eventData.selectedObject.name + " was selected");
     }
 
     public void OnCancel(BaseEventData eventData)
@@ -40,7 +38,7 @@ public class Menu : MonoBehaviour, ISelectHandler, IDeselectHandler, ICancelHand
 
     public void OnDeselect(BaseEventData eventData)
     {
-        //Debug.Log(this.ButtonGameObject.name + " was deselected");
+
     }
 
     public void Ready()
@@ -58,7 +56,6 @@ public class Menu : MonoBehaviour, ISelectHandler, IDeselectHandler, ICancelHand
 
     public void Options()
     {
-        Debug.Log("Options");
         FindObjectOfType<AudioManager>().Play("MenuClick");
     }
 
@@ -66,7 +63,6 @@ public class Menu : MonoBehaviour, ISelectHandler, IDeselectHandler, ICancelHand
     {
         GameObject configManager = GameObject.FindGameObjectWithTag("GameController");
         Destroy(configManager);
-        Debug.Log("Quit");
         Application.Quit();
         FindObjectOfType<AudioManager>().Play("MenuClick");
     }
