5603995692e005d99cb11d3ab750f0ebc0d00557
edwin <Edwinslottje@gmail.com>
2022-06-24 01:17:55 +0200

    Audio and other fixes

--- Year 2 - Project 4/Assets/MainMenuMusic.cs
+++ Year 2 - Project 4/Assets/MainMenuMusic.cs
@@ -7,24 +7,35 @@ public class MainMenuMusic : MonoBehaviour
 {
     public static MainMenuMusic Instance { get; private set; }
     private string currentSceneName;
+    public AudioSource audioSource;
 
     private void Awake()
     {
+
         DontDestroyOnLoad(this);
+        DontDestroyOnLoad(audioSource);
 
         if (Instance != null)
         {
             Debug.Log("[Singleton] Trying to create another instance of singleton");
+            Destroy(audioSource);
+            Destroy(gameObject);
         }
         else
         {
             Instance = this;
             DontDestroyOnLoad(Instance);
         }
+        if (!audioSource.isPlaying)
+        {
+            Debug.Log("is not playing!");
+            audioSource.Play();
+        }
     }
 
     private void FixedUpdate()
     {
+       
         currentSceneName = SceneManager.GetActiveScene().name;
 
         if(currentSceneName == "LevelDesign1" || currentSceneName == "Level2v2" || currentSceneName == "Farm Level")
